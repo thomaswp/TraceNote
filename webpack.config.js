5cf9628a208bd7a28122e0d303ed392fe348c5a7
Thomas Price <twprice@ncsu.edu>
2022-10-27 10:07:40 -0400

    First experiment with synth generation

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+//webpack.config.js
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist/js"),
+    filename: "tracenote.js",
+    library: "TraceNote"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+};
